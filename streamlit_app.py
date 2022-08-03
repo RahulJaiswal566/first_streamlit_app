43a27545544320da78ff10ba2c79c777e0017fc7
RahulJaiswal566 <67284131+RahulJaiswal566@users.noreply.github.com>
2022-08-03 14:27:06 +0530

    Update streamlit_app.py
    added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
 streamlit.title('First Stramlit Use')
 streamlit.header('Second Stramlit Use')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Third Stramlit Use')
