7109cceb585f5aed633fd75c2cc7af6b658cdd7b
RahulJaiswal566 <67284131+RahulJaiswal566@users.noreply.github.com>
2022-08-03 14:27:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 streamlit.title('First Stramlit Use')
-streamlit.header('Second Stramlit Use')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Third Stramlit Use')
